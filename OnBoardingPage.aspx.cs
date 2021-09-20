905a4343dccbcf71484e42ba799dfda982692d5b
Javed Ahmad <v-jaahma@microsoft.com>
2021-09-20 15:27:20 +0530

    updated email alias

--- OnBoardingPage.aspx.cs
+++ OnBoardingPage.aspx.cs
@@ -24,58 +24,64 @@ namespace OnBoardingForm
 
         protected void btnSubmit_Click1(object sender, EventArgs e)
         {
-            var modelTeamName = modeltxtTeamName.Value;
-            var modelPartnerContactMSAlias = modeltxtPartnerAlias.Value;
-            var modelTeamEmail = modelTxtTeamEmail.Value;
-            var modalSuitableIntendedUse = txtSuitabeUse.Value;
-            var modalExternalPartnerAlias = modeltxtExternalPartnerAlias.Value;
-            var modalProjectOverview = modeltxtProjectOverview.Value;
-            var modalTargetDevice = modelTargetDevice.Value;
-            var modalReleaseDate = modelTxtDate.Value;
-            var modalDistributionMethod = modeltxtAreaDistribution.Value;
-            var modalAreaMarket = modeltxtAreaMarket.Value;
-            var modalQPM = modelTxtQPM.Value;
-            var modalQPS = modelTxtQPS.Value;
-            var modalImpAdhereTo = modelTxtImpAdhereTo.Value;
-            var modalConsideredDataTypes = modeltxtAreaDataTypes.Value;
-            var modalFormCode = modelTxtFormCode.Value;
-            var AppIdDetails = modelTxtFormCodeDetail.Value;
-            var modalAPIcalledAt = modeltxtBingAPICalledAt.Value;
-            var modalDetailedScenario = modeltxtDetailedScenario.Value;
-            var myData = new
+            try
             {
-                TeamNameNorganization = modelTeamName,
-                PartnerContactMSAlias = modelPartnerContactMSAlias,
-                TeamEmailAlias = modelTeamEmail,
-                MostSuitableIntendedUse = modalSuitableIntendedUse,
-                ExternalPartnerEmailAlias = modalExternalPartnerAlias,
-                ProjectOverview = modalProjectOverview,
-                TargetDevice = modalTargetDevice,
-                ReleaseDate = modalReleaseDate,
-                DistributionMethod = modalDistributionMethod,
-                AreaMarket = modalAreaMarket,
-                QPM = modalQPM,
-                QPS = modalQPS,
-                AdhereToBingGuideline = modalImpAdhereTo,
-                ConsideredDataTypes = modalConsideredDataTypes,
-                HaveFormCode = modalFormCode,
-                AppId = AppIdDetails,
-                APICalledAt =modalAPIcalledAt,
-                Top2ProjectScenarion=modalDetailedScenario,
-                email = "spbsupp@microsoft.com",
-                Subject = "Test Mail Please Ignore",
-            };
-            string jsonData = JsonConvert.SerializeObject(myData);
-            JObject jsonObject = JObject.Parse(jsonData);
+                var modelTeamName = modeltxtTeamName.Value;
+                var modelPartnerContactMSAlias = modeltxtPartnerAlias.Value;
+                var modelTeamEmail = modelTxtTeamEmail.Value;
+                var modalSuitableIntendedUse = txtSuitabeUse.Value;
+                var modalExternalPartnerAlias = modeltxtExternalPartnerAlias.Value;
+                var modalProjectOverview = modeltxtProjectOverview.Value;
+                var modalTargetDevice = modelTargetDevice.Value;
+                var modalReleaseDate = modelTxtDate.Value;
+                var modalDistributionMethod = modeltxtAreaDistribution.Value;
+                var modalAreaMarket = modeltxtAreaMarket.Value;
+                var modalQPM = modelTxtQPM.Value;
+                var modalQPS = modelTxtQPS.Value;
+                var modalImpAdhereTo = modelTxtImpAdhereTo.Value;
+                var modalConsideredDataTypes = modeltxtAreaDataTypes.Value;
+                var modalFormCode = modelTxtFormCode.Value;
+                var AppIdDetails = modelTxtFormCodeDetail.Value;
+                var modalAPIcalledAt = modeltxtBingAPICalledAt.Value;
+                var modalDetailedScenario = modeltxtDetailedScenario.Value;
+                var myData = new
+                {
+                    TeamNameNorganization = modelTeamName,
+                    PartnerContactMSAlias = modelPartnerContactMSAlias,
+                    TeamEmailAlias = modelTeamEmail,
+                    MostSuitableIntendedUse = modalSuitableIntendedUse,
+                    ExternalPartnerEmailAlias = modalExternalPartnerAlias,
+                    ProjectOverview = modalProjectOverview,
+                    TargetDevice = modalTargetDevice,
+                    ReleaseDate = modalReleaseDate,
+                    DistributionMethod = modalDistributionMethod,
+                    AreaMarket = modalAreaMarket,
+                    QPM = modalQPM,
+                    QPS = modalQPS,
+                    AdhereToBingGuideline = modalImpAdhereTo,
+                    ConsideredDataTypes = modalConsideredDataTypes,
+                    HaveFormCode = modalFormCode,
+                    AppId = AppIdDetails,
+                    APICalledAt = modalAPIcalledAt,
+                    Top2ProjectScenarion = modalDetailedScenario,
+                    email = "spbsupp@microsoft.com;bpgedevops@microsoft.com",
+                    Subject = "Test Mail Please Ignore",
+                };
+                string jsonData = JsonConvert.SerializeObject(myData);
+                JObject jsonObject = JObject.Parse(jsonData);
 
-            using (var client = new HttpClient())
-            {
-                var content = new StringContent(jsonData, Encoding.UTF8, "application/json");
-                var response = client.PostAsync("https://prod-19.centralus.logic.azure.com:443/workflows/11f10a19a36641f898619f931695c6a2/triggers/manual/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=jtdCXT_wmaTuZgIGeWGIIGArEdtHsg2YmrCEegs27ik", content).Result;
+                using (var client = new HttpClient())
+                {
+                    var content = new StringContent(jsonData, Encoding.UTF8, "application/json");
+                    var response = client.PostAsync("https://prod-19.centralus.logic.azure.com:443/workflows/11f10a19a36641f898619f931695c6a2/triggers/manual/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=jtdCXT_wmaTuZgIGeWGIIGArEdtHsg2YmrCEegs27ik", content).Result;
+                }
+
+                Response.Write("<script>alert('Details submitted successfully')</script>");
             }
-
-            Response.Write("<script>alert('Details submitted successfully')</script>");
-
+            catch(Exception ex)
+            {
+                Response.Write("<script>alert('some error ocured. Please contact spbsupp@microsoft.com for support')</script>");
+            }
         }
     }
 }
